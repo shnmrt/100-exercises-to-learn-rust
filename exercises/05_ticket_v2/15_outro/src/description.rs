b37f37a316c91d2f91612732a30eacfa18dd4bab
Murat Şahin <sahinmurat3@gmail.com>
2024-06-01 22:14:07 +0200

    implementation of the description

--- exercises/05_ticket_v2/15_outro/src/description.rs
+++ exercises/05_ticket_v2/15_outro/src/description.rs
@@ -2,7 +2,39 @@
 //   enforcing that the description is not empty and is not longer than 500 bytes.
 //   Implement the traits required to make the tests pass too.
 
+#[derive(Debug, Clone, PartialEq)]
 pub struct TicketDescription(String);
+#[derive(Debug, thiserror::Error)]
+pub enum TicketDescriptionError {
+    #[error("The description cannot be empty")]
+    EmptyDescription,
+    #[error("The description cannot be longer than 500 bytes")]
+    LongDescription
+}
+fn validate(value:&str) -> Result<(), TicketDescriptionError> {
+    if value.is_empty() {
+        Err(TicketDescriptionError::EmptyDescription)
+    } else if value.len() > 500 {
+        Err(TicketDescriptionError::LongDescription)
+    } else {
+        Ok(())
+    }
+}
+
+impl TryFrom<String> for TicketDescription {
+    type Error = TicketDescriptionError;
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        validate(&value.as_str())?;
+        Ok(Self(value))
+    }
+}
+impl TryFrom<&str> for TicketDescription {
+    type Error = TicketDescriptionError;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        validate(value)?;
+        Ok(Self(value.to_string()))
+    }
+}
 
 #[cfg(test)]
 mod tests {
